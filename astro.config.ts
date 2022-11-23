678de2be0731431bfbdcd568a5a92270ab4efc07
迷渡 <justjavac@gmail.com>
2022-11-23 10:15:00 +0800

    添加 rehype-external-links

--- astro.config.ts
+++ astro.config.ts
@@ -3,10 +3,20 @@ import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 import deno from "@astrojs/deno";
 import mdx from "@astrojs/mdx";
+import rehypeExternalLinks from "rehype-external-links";
 
 export default defineConfig({
   site: "https://eso.denohub.com",
-  integrations: [preact(), tailwind(), mdx()],
+  markdown: {
+    rehypePlugins: [
+      [rehypeExternalLinks, { target: "_blank", rel: ["noopener", "noreferrer", "nofollow"] }],
+    ],
+  },
+  integrations: [
+    preact(),
+    tailwind(),
+    // mdx(),
+  ],
   output: "server",
   adapter: deno(),
 });
