0cf94834963e143b6346b0349fac75e1f23679f3
迷渡 <justjavac@gmail.com>
2022-11-23 15:38:26 +0800

    添加贡献者名单

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 上古卷轴 OL 游戏攻略，包括职业、装备、技能、副本、任务、地图、资料片等内容。
 
-Build With:
+本网站使用如下技术构建：
 
 - [Astro](https://astro.build)
 - [Preact](https://preactjs.com)
--- src/consts.ts
+++ src/consts.ts
@@ -8,3 +8,6 @@ export const MAP_SIZE = 1600;
 export const MARKER_SIZE = 64;
 /** CDN 地址 */
 export const CDN_URL = "https://esomap-1251572801.cos.ap-beijing.myqcloud.com";
+
+export const OWNER = "justjavac";
+export const REPO = "esomap";
