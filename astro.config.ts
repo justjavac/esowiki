b2cf0086f737888ba87b16e41c600d54f60e9d56
迷渡 <justjavac@gmail.com>
2022-11-23 09:45:19 +0800

    添加 mdx

--- astro.config.ts
+++ astro.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 import deno from "@astrojs/deno";
+import mdx from "@astrojs/mdx";
 
 export default defineConfig({
   site: "https://eso.denohub.com",
-  integrations: [preact(), tailwind()],
+  integrations: [preact(), tailwind(), mdx()],
   output: "server",
   adapter: deno(),
 });
