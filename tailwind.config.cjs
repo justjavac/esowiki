078847eca781a1314bffdef2c8c6d9890f2b162d
迷渡 <justjavac@gmail.com>
2022-12-07 16:52:06 +0800

    套装搜索

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
   plugins: [
+    require("@tailwindcss/forms"),
     function ({ addVariant }) {
       addVariant("scrollbar", "&::-webkit-scrollbar");
       addVariant("scrollbar-thumb", "&::-webkit-scrollbar-thumb");
