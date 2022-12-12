efe1856f2701f4c30ba295e9b1655e17816a1151
迷渡 <justjavac@gmail.com>
2022-12-12 08:28:10 +0800

    切换 cdn 地址

--- src/consts.ts
+++ src/consts.ts
@@ -7,6 +7,6 @@ export const MAP_SIZE = 1600;
 /** 地图 POI 尺寸 */
 export const MARKER_SIZE = 64;
 /** CDN 地址 */
-export const CDN_URL = "https://esomap-1251572801.cos.ap-beijing.myqcloud.com";
+export const CDN_URL = "https://eso-cdn.denohub.com";
 /** ESO API 地址 */
 export const ESO_API_URL = "https://esoapi.denohub.com";
