8955cb50980c13f44829e25794083f8ce2741427
迷渡 <justjavac@gmail.com>
2022-12-15 11:38:07 +0800

    构建

--- src/utils/md2html.ts
+++ src/utils/md2html.ts
@@ -1,5 +1,20 @@
-import { marked } from "marked";
+import { unified } from "unified";
+import remarkGfm from "remark-gfm";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import rehypeStringify from "rehype-stringify";
+import rehypeRaw from "rehype-raw";
+import rehypeExternalLinks from "rehype-external-links";
 
 export function md2html(md: string): string {
-  return marked(md);
+  const file = unified()
+    .use(remarkParse)
+    .use(remarkGfm)
+    .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeRaw)
+    .use(rehypeExternalLinks, { target: "_blank", rel: ["noopener", "noreferrer", "nofollow"] })
+    .use(rehypeStringify)
+    .processSync(md);
+
+  return file.toString();
 }
