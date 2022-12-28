9fceebed2d67602c8f7a77db8f5afb7ed10250d9
迷渡 <justjavac@gmail.com>
2022-12-28 15:32:49 +0800

    npc

--- src/utils/md2html.ts
+++ src/utils/md2html.ts
@@ -2,7 +2,8 @@ import { micromark } from "micromark";
 import type { HtmlExtension } from "micromark-util-types";
 import { gfm, gfmHtml } from "micromark-extension-gfm";
 
-export function md2html(md: string): string {
+export function md2html(md?: string): string {
+  if (md == null) return "";
   return micromark(md, {
     allowDangerousHtml: true,
     extensions: [gfm()],
