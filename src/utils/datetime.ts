8ed79f843bbef174161fccdef5618566a7edeb57
迷渡 <justjavac@gmail.com>
2023-01-05 16:58:58 +0800

    DLC

--- src/utils/datetime.ts
+++ src/utils/datetime.ts
@@ -6,3 +6,15 @@ export function formatDate(date?: string) {
     day: "numeric",
   }).format(new Date(date));
 }
+
+export function formatDateTime(datetime?: string) {
+  if (datetime == null) return "";
+  return new Intl.DateTimeFormat("zh-CN", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  }).format(new Date(datetime));
+}
