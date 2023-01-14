343f3d621264858c6b5a980668c1a037c6a407c7
迷渡 <justjavac@gmail.com>
2023-01-14 12:42:04 +0800

    markdown book

--- src/utils/md2html.ts
+++ src/utils/md2html.ts
@@ -2,12 +2,24 @@ import { micromark } from "micromark";
 import type { HtmlExtension } from "micromark-util-types";
 import { gfm, gfmHtml } from "micromark-extension-gfm";
 
-export function md2html(md?: string): string {
+export function md2html(md?: string, isBook = false): string {
   if (md == null) return "";
-  return micromark(md.replace(/(?<!\n)\n(?!\n)/g, "<br/>"), {
+
+  if (isBook) {
+    md = md.replace(/(?<!\n)\n(?!\n)/g, "<br/>");
+  }
+
+  return micromark(md, {
     allowDangerousHtml: true,
     extensions: [gfm()],
-    htmlExtensions: [gfmHtml(), externalLinks()],
+    htmlExtensions: [
+      gfmHtml({
+        clobberPrefix: "eso-",
+        label: "脚注",
+        backLabel: "回到内容区域",
+      }),
+      externalLinks(),
+    ],
   });
 }
 
