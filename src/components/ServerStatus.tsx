b9aaa663428c9eaf3fe9fac67e2304570e1343cc
迷渡 <justjavac@gmail.com>
2023-01-18 19:00:53 +0800

    删除冒号

--- src/components/ServerStatus.tsx
+++ src/components/ServerStatus.tsx
@@ -53,7 +53,7 @@ export function ServerStatus(props: ServerStatusProps) {
   return (
     <div class="mx-4 md:mx-0">
       <div class="mt-10 p-4 w-full flex justify-between rounded-md border border-gray-200 text-gray-500 text-sm">
-        <strong>最后更新时间:</strong>
+        <strong>最后更新时间</strong>
         <div class="inline-flex">
           {loading && <Spin />}
           <time dateTime={data.updatedAt}>{localTime}</time>
@@ -81,9 +81,11 @@ export function ServerStatus(props: ServerStatusProps) {
                 </div>
                 <div class="ml-4 flex-shrink-0">
                   <span
-                    class={`font-medium inline-block px-2.5 py-1 rounded-full leading-none ${getStatusColor(
-                      x.status
-                    )}`}
+                    class={`font-medium inline-block px-2.5 py-1 rounded-full leading-none ${
+                      getStatusColor(
+                        x.status,
+                      )
+                    }`}
                   >
                     {getStatusName(x.status)}
                   </span>
@@ -111,12 +113,14 @@ function Spin() {
         r="10"
         stroke="currentColor"
         stroke-width="4"
-      ></circle>
+      >
+      </circle>
       <path
         class="opacity-75"
         fill="currentColor"
         d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-      ></path>
+      >
+      </path>
     </svg>
   );
 }
