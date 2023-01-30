71a042d59cfefc8f58bc04cb824bfa5930438386
迷渡 <justjavac@gmail.com>
2023-01-30 14:50:44 +0800

    price

--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -1,13 +1,27 @@
-import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 import { formatDate } from "@/utils";
 
 type PriceProps = {
   data: any[];
 };
 
+const dtf = new Intl.DateTimeFormat("zh-CN", {
+  month: "long",
+  day: "numeric",
+});
+
 export function Price(props: PriceProps) {
   const data = props.data.map((x) => ({
-    createdAt: formatDate(x.attributes.createdAt),
+    createdAt: dtf.format(new Date(x.attributes.createdAt)),
     平均价格: x.attributes.avg,
     最高价格: x.attributes.max,
     最低价格: x.attributes.min,
@@ -18,8 +32,8 @@ export function Price(props: PriceProps) {
     <ResponsiveContainer>
       <LineChart width={500} height={300} data={data}>
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="createdAt" padding={{ left: 30, right: 30 }} />
-        <YAxis />
+        <XAxis dataKey="createdAt" tickCount={7} />
+        <YAxis domain={["auto", "auto"]} />
         <Tooltip />
         <Legend />
         <Line type="monotone" dataKey="平均价格" stroke="#8884d8" />
